bfcfc30e7b93b6647c19ab6eb591891d1cae82a0
sam <61722729+overnbawatch@users.noreply.github.com>
2022-12-20 20:45:11 +0900

    Add files via upload

--- AtCoder Beginner Contest 282/(B)Let's Get a Perfect Score/main.py
+++ AtCoder Beginner Contest 282/(B)Let's Get a Perfect Score/main.py
@@ -1,4 +1,18 @@
-while True:
-    Alf = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    Inp = int(input())
-    print(Alf[:Inp])
+
+Inp = input().split(' ')
+list = []
+for n in range(int(Inp[0])):
+    list.append(input())
+
+cnt = 0
+for i in range(int(Inp[0]) - 1):
+    for k in range(i + 1, int(Inp[0])):
+        for j in range(int(Inp[1])):
+            if list[i][j] == 'o' or list[k][j] == 'o':
+                if j == int(Inp[1]) - 1:
+                    cnt = cnt + 1
+                continue
+            else:
+                break
+
+print(cnt)
